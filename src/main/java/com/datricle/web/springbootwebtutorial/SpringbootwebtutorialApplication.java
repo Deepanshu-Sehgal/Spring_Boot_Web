37b0954532ce63e93c6fbfdd98ae110b8e5ebf2a
Deepanshu-sehgal <deepanshusehgal.112@gmail.com>
2024-07-06 23:02:07 +0530

    Initial commit

---
+++ src/main/java/com/datricle/web/springbootwebtutorial/SpringbootwebtutorialApplication.java
@@ -0,0 +1,18 @@
+package com.datricle.web.springbootwebtutorial;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebtutorialApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwebtutorialApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello i m running");
+	}
+}
